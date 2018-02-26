ae4a8483af308b2c281105226571252656b45491
AndrianaTodorova <rosiani@mail.bg>
2018-02-26 18:42:13 +0200

    New Exercise Added

---
+++ C#-OOP-Basics/08.InheritanceExercises/03.ManKind/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _03.ManKind
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] infoStudent = Console.ReadLine().Split();
+                string firstName = infoStudent[0];
+                string lastName = infoStudent[1];
+                string facultyNumber = infoStudent[2];
+                Human student = new Student(firstName, lastName, facultyNumber);
+
+                string[] infoWorker = Console.ReadLine().Split();
+                string firstNameWorker = infoWorker[0];
+                string lastNameWorker = infoWorker[1];
+                double salary = double.Parse(infoWorker[2]);
+                double hours = double.Parse(infoWorker[3]);
+                Human worker = new Worker(firstNameWorker, lastNameWorker, salary, hours);
+
+                Console.WriteLine(student.ToString());
+                Console.WriteLine();
+                Console.WriteLine(worker.ToString());
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ C#-OOP-Basics/08.InheritanceExercises/03.ManKind/Worker.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+
+public class Worker : Human
+{
+    public Worker(string firstName, string lastName, double salary, double hours) : base(firstName, lastName)
+    {
+        this.WeekSalary = salary;
+        this.WorkingHours = hours;
+    }
+
+    private double weekSalary;
+
+    public double WeekSalary
+    {
+        get { return this.weekSalary; }
+        set
+        {
+            if (value < 10)
+            {
+                throw new ArgumentException($"Expected value mismatch! Argument: {nameof(this.weekSalary)}");
+            }
+            this.weekSalary = value;
+        }
+    }
+
+    private double workHoursPerDay;
+
+    public double WorkingHours
+    {
+        get { return this.workHoursPerDay; }
+        set
+        {
+            if (value < 1 || value > 12)
+            {
+                throw new ArgumentException($"Expected value mismatch! Argument: {nameof(this.workHoursPerDay)}");
+            }
+            this.workHoursPerDay = value;
+        }
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine(base.ToString())
+            .AppendLine($"Week Salary: {this.WeekSalary:f2}")
+            .AppendLine($"Hours per day: {this.workHoursPerDay:f2}")
+            .Append($"Salary per hour: {(this.WeekSalary / (this.WorkingHours * 5)):f2}");
+        return sb.ToString();
+    }
+}
